f56c34647b1f9e368a39fea97289000dde61ed61
Jack Burnett <jackjburnett@gmail.com>
2024-06-28 11:51:44 +0100

    Converted Parameter Store to JSON

--- modules/generate_stl.py
+++ modules/generate_stl.py
@@ -7,24 +7,24 @@ def generate_button_cap(
     wall_thickness=0.0,
     wall_height=3.0,
     bevel=False,
-    stem_height=4.0,
-    stem_radius=3.0,
-    inner_stem_width=4.2,
-    inner_stem_length=1.4,
+    mount_height=4.0,
+    mount_radius=3.0,
+    mount_cross_width=4.2,
+    mount_cross_length=1.4,
 ):
     # Create the top
     top = cq.Workplane().circle(button_width / 2).extrude(top_thickness)
     # Add bevel
     if bevel:
         top = top.edges().fillet(0.99)
-    # Create the stem
-    inner_stem = (
+    # Create the mount
+    mount_cross = (
         cq.Workplane()
-        .rect(inner_stem_width, inner_stem_length)
-        .rect(inner_stem_length, inner_stem_width)
-        .extrude(stem_height)
+        .rect(mount_cross_width, mount_cross_length)
+        .rect(mount_cross_length, mount_cross_width)
+        .extrude(mount_height)
     )
-    stem = cq.Workplane().circle(stem_radius).extrude(stem_height).cut(inner_stem)
+    mount = cq.Workplane().circle(mount_radius).extrude(mount_height).cut(mount_cross)
     # Create walls
     if wall_thickness > 0:
         walls = (
@@ -34,10 +34,10 @@ def generate_button_cap(
             .extrude(wall_height)
         )
     # Combine all parts
-    cap = cq.Assembly().add(top).add(stem, loc=cq.Location((0, 0, top_thickness)))
+    cap = cq.Assembly().add(top).add(mount, loc=cq.Location((0, 0, top_thickness)))
     if wall_thickness > 0:
         cap.add(walls, loc=cq.Location((0, 0, top_thickness / 2)))
     return cap
 
 
-# generate_button_cap(wall_thickness=1.0).save("cap.step")
+generate_button_cap(wall_thickness=1.0).save("cap.step")
--- parameter_store/switches.py
+++
@@ -1,8 +0,0 @@
-### CHERRY SWITCHES ###
-cherry = {"MX RGB Red": {}, "MX2A RED": {}}
-### GATERON SWITCHES ###
-gateron = {"MX RGB Red": {}, "MX2A RED": {}}
-### KEYCHRON SWITCHES ###
-keychron = {"MX RGB Red": {}, "MX2A RED": {}}
-### KAILH SWITCHES ###
-kailh = {"MX RGB Red": {}, "MX2A RED": {}}
