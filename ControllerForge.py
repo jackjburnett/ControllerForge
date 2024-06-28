544437daef22744faf27b54608fa0ac2b4f5bce3
Jack Burnett <jackjburnett@gmail.com>
2024-06-28 14:38:31 +0100

    Added arguments to ControllerForge

--- ControllerForge.py
+++ ControllerForge.py
@@ -1,17 +1,37 @@
-import json
+import argparse
 
-import modules.generate_stl as generate_stl
-import modules.get_model as get_model
-import modules.train_model as train_model
+from flask import Flask
 
-train_model.train_model()
-with open("parameter_store/switches/kailh.json") as f:
-    kailh = json.load(f)
-generate_stl.generate_button_cap(
-    mount_height=kailh["Red"]["dimensions"]["mount"]["height"],
-    mount_cross_width=kailh["Red"]["dimensions"]["mount"]["cross_width"],
-    mount_cross_length=kailh["Red"]["dimensions"]["mount"]["cross_length"],
-    mount_radius=(kailh["Red"]["dimensions"]["mount"]["cross_width"] / 2) + 1,
-    wall_thickness=1.0,
-).save("generated_files/cap.step")
-get_model.get_model()
+import parameter_store.default_values as default_values
+import utils.pipeline_test as pipeline_test
+
+app = Flask(__name__)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Check for flask app arguments.")
+    parser.add_argument(
+        "--test", "--t", action="store_true", help="Run the pipeline test."
+    )
+    parser.add_argument(
+        "--debug", "--d", action="store_true", help="Run the app with debug mode."
+    )
+    parser.add_argument(
+        "--port", "--p", action="store", help="Set a port number for the flask app."
+    )
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    # Checks if a port was passed, if not defaults it to port 5000
+    if args.port is not None:
+        args.port = int(args.port)
+    else:
+        args.port = default_values.PORT
+    # If --t or --test is given as an argument, run the pipeline test
+    if args.test:
+        print(pipeline_test.pipeline_test())
+    # Default entry is the flask app
+    else:
+        app.run(debug=args.debug, port=args.port)
---
+++ utils/pipeline_test.py
@@ -0,0 +1,25 @@
+import json
+
+import modules.generate_stl as generate_stl
+import modules.get_model as get_model
+import modules.train_model as train_model
+
+
+def pipeline_test():
+    train_model.train_model()
+    with open("parameter_store/switches/kailh.json") as f:
+        kailh = json.load(f)
+    generate_stl.generate_button_cap(
+        mount_height=kailh["Red"]["dimensions"]["mount"]["height"],
+        mount_cross_width=kailh["Red"]["dimensions"]["mount"]["cross_width"],
+        mount_cross_length=kailh["Red"]["dimensions"]["mount"]["cross_length"],
+        mount_radius=(kailh["Red"]["dimensions"]["mount"]["cross_width"] / 2) + 1,
+        wall_thickness=1.0,
+    ).save("generated_files/cap.step")
+
+    get_model.get_model()
+    return "Pipeline Test Complete"
+
+
+if __name__ == "__main__":
+    print("Please test the pipeline via ControllerForge.py")
