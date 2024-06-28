4d59c37cfe04c7bb58f7d7827da631310a5d7b66
Jack Burnett <jackjburnett@gmail.com>
2024-06-28 12:53:52 +0100

    Added button generation from parameter_store

--- ControllerForge.py
+++ ControllerForge.py
@@ -1,5 +1,15 @@
+import json
+
 import modules.generate_stl as generate_stl
 import modules.train_model as train_model
 
 train_model.train_model()
-generate_stl.generate_button_cap(wall_thickness=1.0).save("generated_files/cap.step")
+with open("parameter_store/switches/kailh.json") as f:
+    kailh = json.load(f)
+generate_stl.generate_button_cap(
+    mount_height=kailh["Red"]["dimensions"]["mount"]["height"],
+    mount_cross_width=kailh["Red"]["dimensions"]["mount"]["cross_width"],
+    mount_cross_length=kailh["Red"]["dimensions"]["mount"]["cross_length"],
+    mount_radius=(kailh["Red"]["dimensions"]["mount"]["cross_width"] / 2) + 1,
+    wall_thickness=1.0,
+).save("generated_files/cap.step")
