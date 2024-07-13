5042f5c1f6383e7df5bc1eb224d56997c0859caa
Jack Burnett <jackjburnett@gmail.com>
2024-07-13 15:33:19 +0100

    Added checkKSD

--- utils/SetKICAD_SYMBOL_DIR.py
+++ utils/SetKICAD_SYMBOL_DIR.py
@@ -9,5 +9,13 @@ def SetKSD(KCDPath=r"C:\Program Files\KiCad\8.0\share\kicad\symbols"):
     return "KICAD_SYMBOL_DIR set to:", os.environ["KICAD_SYMBOL_DIR"]
 
 
+def CheckKSD():
+    if os.environ.get("KICAD_SYMBOL_DIR") is None:
+        return False
+    else:
+        return True
+
+
 if __name__ == "__main__":
     print(SetKSD())
+    print(CheckKSD())
